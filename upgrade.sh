686097828c2ae9724961f7d06bdb7a7cd2bf7289
Marcos de Benedicto <mbenedicto@gmail.com>
2017-08-05 11:05:24 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 git init
 git add *
-git commit -m "commit $(date +%Y%m%d)"
+git commit -m "commit $(date +%Y%m%d-%H%M%S)"
 git remote add origin https://github.com/mbenedicto99/website.express.git
 git push -u origin master
