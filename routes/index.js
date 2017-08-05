cf7ff824e092ab73b1d354a3c135768fb22dad64
mbenedicto99 <mbenedicto@gmail.com>
2017-08-05 08:42:13 -0300

    commit 20170805

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'eboxx no Express' });
+});
+
+/* GET new page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Novo Cadastro' });
+});
+
+/* POST new page. */
+router.post('/new', function(req, res, next) {
+	var nome = req.body.nome;
+	var idade = req.body.nome;
+  res.redirect('/?nome=' + nome);
+});
+
+module.exports = router;
