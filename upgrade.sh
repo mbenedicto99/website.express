7d231e7a8f38a035099f166b75fa7c44b775e21a
mbenedicto99 <mbenedicto@gmail.com>
2017-08-05 11:00:20 -0300

    commit 20170805

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 git init
 git add *
 git commit -m "commit $(date +%Y%m%d)"
-#git remote add origin https://github.com/mbenedicto99/NodeJS.git #PARA CRIAR NOVO REPOP
-git push origin master
+git remote add origin https://github.com/mbenedicto99/NodeJS.git #PARA CRIAR NOVO REPOP
+git push -u origin master
